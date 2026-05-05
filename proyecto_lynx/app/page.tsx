2e4c1ea0e055be65b2df7a47c2d3f3436fac932d
FernandoMR10 <fernando.martinezr28@gmail.com>
2026-05-05 14:16:39 +0000

    Se agrego la seccion Tutores

--- proyecto_lynx/app/page.tsx
+++ proyecto_lynx/app/page.tsx
@@ -3,15 +3,16 @@ import Header from "./header/header";
 import Calendario from "./calendario/calendario";
 import PanelAlumno from "./PanelAlumno/PanelAlumno";
 import ProxAct from "./ProxAct/ProxAct";
+import Tutores from "./Tutores/Tutores";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Calendario />
       <PanelAlumno />
+      <Calendario />
       <ProxAct />
+      <Tutores />
     </>
   );
-}
 }
